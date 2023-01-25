ea81aa15edf05704870fabadc05d3884305b594d
Reni Somoye <rensomoye@gmail.com>
2023-01-24 16:12:42 -0800

    commit tree 90d0b769ba4b9a82753106d3a5b10ef118ea7a11

--- index.js
+++ index.js
@@ -12,5 +12,8 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(title => {
+      return title.split(" ").map(word => word[0].toUpperCase() + word.slice(1)).join(" ");
+  });
 }
+//console.log(titleCased());
